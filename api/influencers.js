25f68e09744098a1a6b95b357de2ae3d47f1b263
Tejas <tejasgulati11227@gmail.com>
2025-07-30 16:58:15 +0530

    added stats

--- api/influencers.js
+++ api/influencers.js
@@ -44,7 +44,7 @@ export default async function handler(req, res) {
           username,
           robust_tier_adjusted_engagement_rate,
           credibility_score->>'value' AS credibility_score,
-          account_tier,
+          account_tier
         FROM scrapped.instagram_profile_analysis
       `);
       return res.status(200).json(result.rows);
